7ae20d6ccbfa4ad8d4a9bd94fd2f438fb5a09bab
Lalit <lt72@rediffmail.com>
2022-07-15 21:00:55 +0530

    initial commit

--- init_setup.sh
+++ init_setup.sh
@@ -4,9 +4,9 @@ echo [$(date)]:"creating environment with python ${_VERSION_}"
 conda create --prefix ./env pythone=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
-echo [$(date)]:"install requirements
+echo [$(date)]:"install requirements"
 pip install -r requirements.txt
-echo [$(date)]:'END'
+echo [$(date)]:"END"
 
 # to remove everything - 
 # rm -rf env/ .gitignore conda.yaml README.md.git/
